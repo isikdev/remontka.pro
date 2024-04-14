065391d5d44e6531e6f4f5a42302d616b158c4c9
Iskandar <isikjandev@gmail.com>
2024-04-14 23:34:15 +0500

    101%

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$('.reviews__slick-wrapper').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.slick__prev'),
+    nextArrow: $('.slick__next'),
+});
+$(document).ready(function () {
+    $('.question__acc-body').slideUp();
+    $('.question__acc-head').click(function () {
+        var $accBody = $(this).next('.question__acc-body');
+        $accBody.slideToggle();
+        $(this).find('.arrow').toggleClass('down');
+        $('.question__acc-body').not($accBody).slideUp();
+        $('.question__acc-head').not($(this)).find('.arrow').removeClass('down'); 
+    });
+});
