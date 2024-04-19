da72f3b39b09c04e4d8fe2739e290adacd21456c
Iskandar <isikjandev@gmail.com>
2024-04-20 00:08:35 +0500

    101%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
-$('.reviews__slick-wrapper').slick(
-    {
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: $('.slick__prev'),
-        nextArrow: $('.slick__next'),
-    }
-);
+$('.reviews__slick-wrapper').slick({
+    infinite: true,
+    arrows: true,
+    prevArrow: $('.slick__prev'),
+    nextArrow: $('.slick__next'),
+});
+
 $('.reviews__slick-wrapper-video').slick(
     {
         infinite: true,
@@ -28,30 +25,28 @@ $(document).ready(function () {
         $('.question__acc-head').not($(this)).find('.arrow').removeClass('down');
     });
 });
-$(document).ready(function () {
-    var minutes = parseInt($('.time span:eq(0)').text());
-    var seconds = parseInt($('.time span:eq(1)').text());
-    var totalSeconds = minutes * 60 + seconds;
-    var timer = setInterval(function () {
-        totalSeconds--;
-        if (totalSeconds < 0) {
-            clearInterval(timer);
-        }
-        else {
-            var minutesLeft = Math.floor(totalSeconds / 60);
-            var secondsLeft = totalSeconds % 60;
-            $('.time span:eq(0)').text(minutesLeft);
-            $('.time span:eq(1)').text(secondsLeft);
-        }
-    }, 1000);
-});
+
 $(".get__popup").click(function () {
     $(".modalbox").toggleClass("modalbox__active");
+});
+$(".get__price-popup").click(function () {
+    var serviceName = $(this).find("td:eq(2)").text();
+    $(".service__title").text(serviceName);
+    $(".modalbox__price").toggleClass("modalbox__price-active");
 });
 $(".modal__send").submit(function (event) {
     event.preventDefault();
     $(".modalbox__second").fadeIn();
 });
+$(".get__free-diagnostic").click(function () {
+    $(".modalbox__diagnostics").toggleClass("modalbox__diagnostics-active")
+})
+$(".get__master").click(function () {
+    $(".modalbox__master").toggleClass("modalbox__master-active")
+})
+$(".get__review").click(function () {
+    $(".modalbox__review").toggleClass("modalbox__review-active")
+})
 $(".open__tel").click(function () {
     $(this).css("display", "none")
 })
@@ -75,33 +70,26 @@ $('.find__map-tel-input').hide();
 $('.find__map-tel-btn').prop('disabled', true);
 
 $(document).ready(function () {
-    // Функция, которая будет вызываться при изменении значения первого поля ввода
     $('input[placeholder="Укажите ваш адрес"]').on('input', function () {
         var addressValue = $(this).val();
 
-        // Проверяем, содержит ли первое поле ввода значение
         if (addressValue) {
-            // Если содержит, то показываем второе поле ввода
             $('.find__map-tel-input').show();
         } else {
-            // Если не содержит, то скрываем второе поле ввода
             $('.find__map-tel-input').hide();
         }
     });
 
-    // Функция, которая будет вызываться при изменении значений обоих полей ввода
     function checkFields() {
         var addressValue = $('input[placeholder="Укажите ваш адрес"]').val();
         var telValue = $('.find__map-tel-input').val();
 
-        // Проверка наличия значений в обоих полях
         if (addressValue && telValue) {
-            $('.btn__red').prop('disabled', false); // Включаем кнопку
+            $('.btn__red').prop('disabled', false);
         } else {
-            $('.btn__red').prop('disabled', true); // Выключаем кнопку
+            $('.btn__red').prop('disabled', true);
         }
     }
 
-    // Привязываем функцию к событиям изменения значений в полях ввода
     $('input[placeholder="Укажите ваш адрес"], .find__map-tel-input').on('input', checkFields);
 });
